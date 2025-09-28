72a5b710fff0a26f91f7653e3aeb78e6af0246e7
gkkconan <conan.gkk@gmail.com>
2025-09-29 01:51:14 +0200

    feat(server): add cookie parser

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.js";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -10,8 +11,9 @@ const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
+app.use(cookieParser());
 
 // Routes
 app.use("/auth", authRoutes);
