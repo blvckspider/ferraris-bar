920634dddcb48d7155ae838641313c1f5d620cad
gkkconan <conan.gkk@gmail.com>
2025-09-29 02:05:05 +0200

    fix(server): update .env file path

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,7 +8,7 @@ import authRoutes from "./routes/auth.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-dotenv.config({ path: path.resolve(__dirname, "../.env") });
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
 
 const app = express();
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
